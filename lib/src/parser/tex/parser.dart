9bc4434e55b3850e6e28efdeb461109939500a12
Dennis Kugelmann <dennis.kugelmann@simpleclub.com>
2021-09-20 08:38:41 +0200

    Pass text/math mode properly to SymbolNode (Proper italics from umlauts in text mode) (#9)
    * Pass mode properly to symbols to support umlauts in text mode
    
    * Fix formatting
    
    * Fix iOS build settings

--- lib/src/parser/tex/parser.dart
+++ lib/src/parser/tex/parser.dart
@@ -451,6 +451,7 @@ class TexParser {
   }
 
   final argParsingContexts = Queue<ArgumentParsingContext>();
+
   ArgumentParsingContext get currArgParsingContext => argParsingContexts.last;
 
   void _enterArgumentParsingMode(String name, FunctionSpec funcData) {
@@ -477,6 +478,7 @@ class TexParser {
       r'^#?([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})$',
       caseSensitive: false);
   static final _parseColorRegex3 = RegExp(r'^([a-z]+)$', caseSensitive: false);
+
   // static final _parseColorRegex =
   //     RegExp(r'^(#[a-f0-9]{3}|#?[a-f0-9]{6}|[a-z]+)$', caseSensitive: false);
   // static final _matchColorRegex =
@@ -531,6 +533,7 @@ class TexParser {
       RegExp(r'^[-+]? *(?:$|\d+|\d+\.\d*|\.\d*) *[a-z]{0,2} *$');
   static final _parseMeasurementRegex =
       RegExp(r'([-+]?) *(\d+(?:\.\d*)?|\.\d+) *([a-z]{2})');
+
   Measurement? parseArgSize({required bool optional}) {
     currArgParsingContext.newArgument(optional: optional);
     final i = currArgParsingContext.currArgNum;
@@ -703,6 +706,7 @@ class TexParser {
   }
 
   static final _parseVerbRegex = RegExp(r'^\\verb[^a-zA-Z]');
+
   GreenNode? _parseSymbol() {
     final nucleus = this.fetch();
     var text = nucleus.text;
@@ -792,7 +796,9 @@ class TexParser {
             'Unicode text character "${text[0]} used in math mode"', nucleus);
       }
       symbol = SymbolNode(
-          symbol: text + combiningMarks, overrideAtomType: AtomType.ord);
+          symbol: text + combiningMarks,
+          overrideAtomType: AtomType.ord,
+          mode: mode);
     } else {
       return null;
     }
@@ -818,6 +824,7 @@ class ArgumentParsingContext {
 
   bool get optional => _optional;
   bool _optional;
+
   set optional(bool value) {
     assert(_optional || !value);
     _optional = value;
@@ -842,6 +849,7 @@ class ScriptsParsingResults {
   final EquationRowNode? subscript;
   final EquationRowNode? superscript;
   final bool? limits;
+
   const ScriptsParsingResults({
     required this.subscript,
     required this.superscript,
