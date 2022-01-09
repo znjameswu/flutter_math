08b607ac1523363a79f88981799164e670d9c303
StroeAndreX <stroeandrex.studio@gmail.com>
2022-01-09 15:32:48 +0100

    Big Fix new flutter

--- lib/src/widgets/selectable.dart
+++ lib/src/widgets/selectable.dart
@@ -562,6 +562,27 @@ class InternalSelectableMathState extends State<InternalSelectableMath>
       TextEditingValue value, SelectionChangedCause cause) {
     // TODO: implement userUpdateTextEditingValue
   }
+
+  @override
+  void copySelection(SelectionChangedCause cause) {
+    // TODO: implement copySelection
+  }
+
+  @override
+  void cutSelection(SelectionChangedCause cause) {
+    // TODO: implement cutSelection
+  }
+
+  @override
+  Future<void> pasteText(SelectionChangedCause cause) {
+    // TODO: implement pasteText
+    throw UnimplementedError();
+  }
+
+  @override
+  void selectAll(SelectionChangedCause cause) {
+    // TODO: implement selectAll
+  }
 }
 
 class SelectionStyle {
